19904abe9667a45be8d87cf8cfcb45e0e279f05f
flood <teranys1986@gmail.com>
2023-07-06 11:06:56 +0300

    добавил модели целий в админку и дописал реадми

--- goals/admin.py
+++ goals/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 
-from goals.models import GoalCategory
+from goals.models import GoalCategory, Goal, GoalComment
+
 
 class GoalCategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created', 'updated')
     search_fields = ('title', 'user')
+    list_filter = ('is_deleted')
+
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'user', 'created', 'updated')
+    search_fields = ('title', 'user')
+    list_filter = ('status')
+
+class GoalCommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'goal', 'created', 'updated')
+    search_fields = ('user', 'goal')
 
 
 admin.site.register(GoalCategory, GoalCategoryAdmin)
+admin.site.register(Goal, GoalAdmin)
+admin.site.register(GoalComment, GoalCommentAdmin)
